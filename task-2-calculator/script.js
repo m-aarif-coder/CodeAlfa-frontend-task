09ae19f684fb3ece0bcca7e2495b469578416bfd
m-aarif-coder <shinghkabir120@gmail.com>
2026-09-03 17:27:58 +0530

    Update script.js

--- task-2-calculator/script.js
+++ task-2-calculator/script.js
@@ -1,58 +1,21 @@
-// Scroll To Top Button
+const display = document.getElementById("display");
 
-const scrollTop = document.getElementById("scrollTop");
+function appendValue(value) {
+    display.value += value;
+}
 
-window.addEventListener("scroll", function () {
+function clearDisplay() {
+    display.value = "";
+}
 
-    if (window.scrollY > 400) {
-        scrollTop.style.display = "block";
-    } else {
-        scrollTop.style.display = "none";
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
     }
-
-});
-
-scrollTop.addEventListener("click", function () {
-
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-
-});
-
-
-// Navbar Active Link
-
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-links a");
-
-window.addEventListener("scroll", function () {
-
-    let current = "";
-
-    sections.forEach(section => {
-
-        const sectionTop = section.offsetTop - 150;
-        const sectionHeight = section.clientHeight;
-
-        if (window.scrollY >= sectionTop &&
-            window.scrollY < sectionTop + sectionHeight) {
-
-            current = section.getAttribute("id");
-
-        }
-
-    });
-
-    navLinks.forEach(link => {
-
-        link.classList.remove("active");
-
-        if (link.getAttribute("href") === "#" + current) {
-            link.classList.add("active");
-        }
-
-    });
-
-});
+}
